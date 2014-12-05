b5a2b96806a1cc0c8fec6d96fe9d4b059743eeab
antonokulov <qantus@mail.ru>
2014-12-05 10:16:04 +0300

    Events fix

--- app/config/events.php
+++ app/config/events.php
@@ -11,15 +11,12 @@ return [
 //        'postAction',
 //        ['\Mindy\Base\Controller', 'postAction']
 //    ],
-    [
-        '\Mindy\Orm\Orm', 'beforeValidate', ['\Mindy\Orm\Orm', 'beforeValidate'],
-        '\Mindy\Orm\Orm', 'afterValidate', ['\Mindy\Orm\Orm', 'afterValidate'],
-        '\Mindy\Orm\Orm', 'beforeSave', ['\Mindy\Orm\Orm', 'beforeSave'],
-        '\Mindy\Orm\Orm', 'afterSave', ['\Mindy\Orm\Orm', 'afterSave'],
-        '\Mindy\Orm\Orm', 'beforeDelete', ['\Mindy\Orm\Orm', 'beforeDelete'],
-        '\Mindy\Orm\Orm', 'afterDelete', ['\Mindy\Orm\Orm', 'afterDelete'],
-
-        '\Mindy\Form\BaseForm', 'beforeValidate', ['\Mindy\Orm\Orm', 'beforeValidate'],
-        '\Mindy\Form\BaseForm', 'afterValidate', ['\Mindy\Orm\Orm', 'afterValidate'],
-    ],
+    ['\Mindy\Orm\Orm', 'beforeValidate', ['\Mindy\Orm\Orm', 'beforeValidateExternal']],
+    ['\Mindy\Orm\Orm', 'afterValidate', ['\Mindy\Orm\Orm', 'afterValidateExternal']],
+    ['\Mindy\Orm\Orm', 'beforeSave', ['\Mindy\Orm\Orm', 'beforeSaveExternal']],
+    ['\Mindy\Orm\Orm', 'afterSave', ['\Mindy\Orm\Orm', 'afterSaveExternal']],
+    ['\Mindy\Orm\Orm', 'beforeDelete', ['\Mindy\Orm\Orm', 'beforeDeleteExternal']],
+    ['\Mindy\Orm\Orm', 'afterDelete', ['\Mindy\Orm\Orm', 'afterDeleteExternal']],
+    ['\Mindy\Form\BaseForm', 'beforeValidate', ['\Mindy\Orm\Orm', 'beforeValidateExternal']],
+    ['\Mindy\Form\BaseForm', 'afterValidate', ['\Mindy\Orm\Orm', 'afterValidateExternal']],
 ];
