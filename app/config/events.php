008f667bfff5a33b95df9107e5410b2ce9c96e32
Maxim <max@studio107.ru>
2014-12-07 16:13:27 +0300

    Update events.php

--- app/config/events.php
+++ app/config/events.php
@@ -1,22 +1,37 @@
 <?php
 
 return [
-//    [
-//        '\Mindy\Base\Controller',
-//        'preAction',
-//        ['\Mindy\Base\Controller', 'preAction']
-//    ],
-//    [
-//        '\Mindy\Base\Controller',
-//        'postAction',
-//        ['\Mindy\Base\Controller', 'postAction']
-//    ],
-    ['\Mindy\Orm\Orm', 'beforeValidate', ['\Mindy\Orm\Orm', 'beforeValidateExternal']],
-    ['\Mindy\Orm\Orm', 'afterValidate', ['\Mindy\Orm\Orm', 'afterValidateExternal']],
-    ['\Mindy\Orm\Orm', 'beforeSave', ['\Mindy\Orm\Orm', 'beforeSaveExternal']],
-    ['\Mindy\Orm\Orm', 'afterSave', ['\Mindy\Orm\Orm', 'afterSaveExternal']],
-    ['\Mindy\Orm\Orm', 'beforeDelete', ['\Mindy\Orm\Orm', 'beforeDeleteExternal']],
-    ['\Mindy\Orm\Orm', 'afterDelete', ['\Mindy\Orm\Orm', 'afterDeleteExternal']],
-    ['\Mindy\Form\BaseForm', 'beforeValidate', ['\Mindy\Orm\Orm', 'beforeValidateExternal']],
-    ['\Mindy\Form\BaseForm', 'afterValidate', ['\Mindy\Orm\Orm', 'afterValidateExternal']],
+    [
+        '\Mindy\Orm\Orm', 'beforeValidate',
+        ['\Mindy\Orm\Orm', 'beforeValidate']
+    ],
+    [
+        '\Mindy\Orm\Orm', 'afterValidate',
+        ['\Mindy\Orm\Orm', 'afterValidate']
+    ],
+    [
+        '\Mindy\Orm\Orm', 'beforeSave',
+        ['\Mindy\Orm\Orm', 'beforeSave']
+    ],
+    [
+        '\Mindy\Orm\Orm', 'afterSave',
+        ['\Mindy\Orm\Orm', 'afterSave']
+    ],
+    [
+        '\Mindy\Orm\Orm', 'beforeDelete',
+        ['\Mindy\Orm\Orm', 'beforeDelete']
+    ],
+    [
+        '\Mindy\Orm\Orm', 'afterDelete',
+        ['\Mindy\Orm\Orm', 'afterDelete']
+    ],
+
+    [
+        '\Mindy\Form\BaseForm', 'beforeValidate',
+        ['\Mindy\Form\BaseForm', 'beforeValidate']
+    ],
+    [
+        '\Mindy\Form\BaseForm', 'afterValidate',
+        ['\Mindy\Form\BaseForm', 'afterValidate']
+    ],
 ];
