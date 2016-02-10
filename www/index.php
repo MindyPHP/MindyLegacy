e964425647542df71745e50d597ca73c9c577baf
Max <max@studio107.ru>
2016-02-10 14:59:22 +0300

    update index fiel

--- www/index.php
+++ www/index.php
@@ -2,16 +2,15 @@
 
 defined('MINDY_PATH') or define('MINDY_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
-$debug = true;
+$config = MINDY_PATH . '../app/config/settings';
+$debug = is_file($config . '_local.php');
+
 if ($debug) {
     defined('MINDY_DEBUG') or define('MINDY_DEBUG', true);
     defined('MINDY_TRACE_LEVEL') or define('MINDY_TRACE_LEVEL', 3);
     defined('MINDY_ENABLE_ERROR_HANDLER') or define('MINDY_ENABLE_ERROR_HANDLER', true);
     defined('MINDY_ENABLE_EXCEPTION_HANDLER') or define('MINDY_ENABLE_EXCEPTION_HANDLER', true);
     ini_set('error_reporting', -1);
-}
-$config = MINDY_PATH . '../app/config/settings';
-if (is_file($config . '_local.php')) {
     $config .= '_local';
 }
 
